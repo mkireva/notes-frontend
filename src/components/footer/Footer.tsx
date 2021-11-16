39de188883e6eb44161b3df67dd3feed1dc992f8
Michael Mannseicher <m.mansi1@gmx.net>
2021-11-16 17:27:25 +0100

    fix: dont update language if nothing changed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,40 +1,43 @@
 import Toolbar from "@material-ui/core/Toolbar";
-import React from "react";
-import { Button } from "@material-ui/core";
-import { useTranslation } from "react-i18next";
-import { getFooterClasses } from "./Footer.jss";
+import React, {useCallback} from "react";
+import {Button} from "@material-ui/core";
+import {useTranslation} from "react-i18next";
+import {getFooterClasses} from "./Footer.jss";
 
 export const Footer = () => {
-  const classes = getFooterClasses();
+    const classes = getFooterClasses();
 
-  const { i18n } = useTranslation();
-  const changeLanguage = (lng: string) => {
-    i18n.changeLanguage(lng);
-  };
+    const {i18n} = useTranslation();
+    
+    const changeLanguage = useCallback((lng: string) => {
+        if (i18n.language !== lng) {
+            i18n.changeLanguage(lng);
+        }
+    }, [i18n]);
 
-  return (
-    <footer className={classes.root}>
-      <Toolbar className={classes.footerText}>
-        Copyright &copy; 2022, Beinsa Notes
-        <Button
-          className={classes.footerButton}
-          onClick={() => changeLanguage("de")}
-        >
-          DE
-        </Button>
-        <Button
-          className={classes.footerButton}
-          onClick={() => changeLanguage("en")}
-        >
-          EN
-        </Button>
-        <Button
-          className={classes.footerButton}
-          onClick={() => changeLanguage("bg")}
-        >
-          BG
-        </Button>
-      </Toolbar>
-    </footer>
-  );
+    return (
+        <footer className={classes.root}>
+            <Toolbar className={classes.footerText}>
+                Copyright &copy; 2022, Beinsa Notes
+                <Button
+                    className={classes.footerButton}
+                    onClick={() => changeLanguage("de")}
+                >
+                    DE
+                </Button>
+                <Button
+                    className={classes.footerButton}
+                    onClick={() => changeLanguage("en")}
+                >
+                    EN
+                </Button>
+                <Button
+                    className={classes.footerButton}
+                    onClick={() => changeLanguage("bg")}
+                >
+                    BG
+                </Button>
+            </Toolbar>
+        </footer>
+    );
 };
