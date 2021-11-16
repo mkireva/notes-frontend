a8a08d2ce8b3db5353a356b447b70d97eaf3686e
Michael Mannseicher <m.mansi1@gmx.net>
2021-11-16 17:22:59 +0100

    fix: gb language

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,42 @@
-import React, { memo, Suspense } from "react";
-import { I18nextProvider, useTranslation } from "react-i18next";
+import React, { Suspense} from "react";
+import {I18nextProvider, useTranslation} from "react-i18next";
 import de from "./assets/i18n/de.json";
 import en from "./assets/i18n/en.json";
 import bg from "./assets/i18n/bg.json";
-import { initI18n } from "./plugins/i18n";
-import { ThemeProvider } from "./theme/ThemeProvider";
-import { StyledEngineProvider } from "@mui/material/styles";
-import { CircularProgress, CssBaseline } from "@mui/material";
-import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
-import { PageRouter } from "./pages/PageRouter";
-import { Loading } from "./components/Loading";
-import { StylesProvider } from "@mui/styles";
+import {initI18n} from "./plugins/i18n";
+import {ThemeProvider} from "./theme/ThemeProvider";
+import {StyledEngineProvider} from "@mui/material/styles";
+import {CssBaseline} from "@mui/material";
+import {Router} from "react-router-dom";
+import {createBrowserHistory} from "history";
+import {PageRouter} from "./pages/PageRouter";
 
 initI18n(
-  {
-    de,
-    en,
-    bg,
-  },
-  "en"
+    {
+        de,
+        en,
+        bg,
+    },
+    "en"
 );
 
 const history = createBrowserHistory();
-const App = () => {
-  const { i18n } = useTranslation();
+export const App = () => {
+    const {i18n} = useTranslation();
 
-  //lazy loading require suspense
-  return (
-    <I18nextProvider i18n={i18n}>
-      <CssBaseline />
-      <StylesProvider>
-        <StyledEngineProvider injectFirst>
-          <ThemeProvider>
-            <Suspense fallback={null}>
-              <Router history={history}>
-                <PageRouter />
-              </Router>
-            </Suspense>
-          </ThemeProvider>
-        </StyledEngineProvider>
-      </StylesProvider>
-    </I18nextProvider>
-  );
+    //lazy loading require suspense
+    return (
+        <I18nextProvider i18n={i18n}>
+            <CssBaseline/>
+                <StyledEngineProvider injectFirst>
+                    <ThemeProvider>
+                        <Suspense fallback={null}>
+                            <Router history={history}>
+                                <PageRouter/>
+                            </Router>
+                        </Suspense>
+                    </ThemeProvider>
+                </StyledEngineProvider>
+        </I18nextProvider>
+    );
 };
-
-export default memo(App);
