663a795c365b526175755147b3b4aca16b8cd8f0
Michael Mannseicher <m.mansi1@gmx.net>
2021-11-16 18:54:05 +0100

    fix: bug in rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense} from "react";
+import React, {Suspense} from "react";
 import {I18nextProvider, useTranslation} from "react-i18next";
 import de from "./assets/i18n/de.json";
 import en from "./assets/i18n/en.json";
@@ -27,16 +27,16 @@ export const App = () => {
     //lazy loading require suspense
     return (
         <I18nextProvider i18n={i18n}>
-            <CssBaseline/>
-                <StyledEngineProvider injectFirst>
-                    <ThemeProvider>
-                        <Suspense fallback={null}>
-                            <Router history={history}>
-                                <PageRouter/>
-                            </Router>
-                        </Suspense>
-                    </ThemeProvider>
-                </StyledEngineProvider>
+            <StyledEngineProvider injectFirst>
+                <ThemeProvider>
+                    <CssBaseline/>
+                    <Suspense fallback={null}>
+                        <Router history={history}>
+                            <PageRouter/>
+                        </Router>
+                    </Suspense>
+                </ThemeProvider>
+            </StyledEngineProvider>
         </I18nextProvider>
     );
 };
