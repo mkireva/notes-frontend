bf3c19cb7f8344068c870db70101fcdc2ef8f2f9
mkireva <mariya@kireva.de>
2021-12-02 19:09:43 +0100

    add new sapleFile

--- src/pages/score/ScorePage.tsx
+++ src/pages/score/ScorePage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Osmd } from "../../lib/Osmd";
 // TODO: in future load it from some backend
-import sampleFile from "../../assets/music/igrata-na-poto4eto.musicxml";
+import sampleFile from "../../assets/music/Vehadi.musicxml";
 import { InfoBox } from "../../components/infobox/InfoBox";
 import { Box, Grid } from "@mui/material";
 
@@ -12,7 +12,7 @@ export const ScorePage = () => {
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={3}>
           <Grid item xs></Grid>
-          <Grid item xs={8}>
+          <Grid item xs={10}>
             <Osmd file={sampleFile} />
           </Grid>
           <Grid item xs></Grid>
