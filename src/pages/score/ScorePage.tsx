20eb8a55686782119266491eacec803318198516
mkireva <mariya@kireva.de>
2021-12-13 13:54:40 +0100

    add file options

--- src/pages/score/ScorePage.tsx
+++ src/pages/score/ScorePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Osmd } from "../../lib/Osmd";
 // TODO: in future load it from some backend
 import sampleFile from "../../assets/music/Vehadi.musicxml";
@@ -6,6 +6,20 @@ import { InfoBox } from "../../components/infobox/InfoBox";
 import { Box, Grid } from "@mui/material";
 
 export const ScorePage = () => {
+  const [files, setFiles] = useState([
+    {
+      label: "Beethoven_AnDieFerneGeliebte",
+      value: "Beethoven_AnDieFerneGeliebte.xml",
+    },
+    {
+      label: "Vehadi",
+      value: "Vehadi.musicxml",
+    },
+  ]);
+
+  const handleClick = (e) => {
+    setFiles({ files: e.target.value });
+  };
   return (
     <div className="ScorePage">
       <InfoBox />
@@ -13,6 +27,13 @@ export const ScorePage = () => {
         <Grid container spacing={3}>
           <Grid item xs></Grid>
           <Grid item xs={10}>
+            <select handleClick={handleClick}>
+              {files.map((file) => (
+                <option key={file.label} value={file.value}>
+                  {file.value}
+                </option>
+              ))}
+            </select>
             <Osmd file={sampleFile} />
           </Grid>
           <Grid item xs></Grid>
